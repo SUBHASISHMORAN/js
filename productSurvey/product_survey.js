14d6253c2983ca16929320450445f1acd92225dd
SUBHASISHMORAN <subhasishmoran00@gmail.com>
2025-11-20 19:21:47 +0530

    Added survey form JS functions

---
+++ productSurvey/product_survey.js
@@ -0,0 +1,35 @@
+function submitFeedback() {
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+
+    // Show thank you message
+    alert('Thank you for your valuable feedback');
+
+    // Display user feedback on webpage
+    document.getElementById('userName').innerHTML = username;
+    document.getElementById('userAge').innerHTML = age;
+    document.getElementById('userEmail').innerHTML = email;
+    document.getElementById('userJob').innerHTML = job;
+    document.getElementById('userDesignation').innerHTML = designation;
+    document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userFeedback').innerHTML = feedback;
+
+    // Make hidden div visible
+    document.getElementById('userInfo').style.display = 'block';
+}
+
+// Outside the function
+const submitButton = document.getElementById('submitBtn');
+submitButton.onclick = submitFeedback;
+
+// Step 4: Submit when pressing Enter
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        submitFeedback();
+    }
+});
